6f005e5c3f7d2c9c2f931706126aaafd627e8b85
andrewbrozek <brozek.andrew@gmail.com>
2013-06-06 21:13:34 +0200

    add order details transaction

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -5,5 +5,13 @@ class OrderDetail < ActiveRecord::Base
   belongs_to :order, foreign_key: "OrderID"
   belongs_to :product, foreign_key: "ProductID" 
 
-
+	def self.new_order(quantity, unit_price, discount, product_id)
+			ActiveRecord::Base.transaction do
+ 		 	order = Order.create()
+  		order_detail = OrderDetail.new(:Discount => discount, :Quantity => quantity, :UnitPrice => unit_price)
+  		order_detail.ProductID = Product.find(product_id).ProductID
+  		order_detail.OrderID = order.OrderID
+  		order_detail.save
+		end
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,10 @@ class Product < ActiveRecord::Base
 
   has_many :order_details, foreign_key: "ProductID"
   has_many :orders, through: :order_details
-
+	
+	def self.make_an_order(product_id, amount)
+	  product = Product.find(product_id)
+		product.update_attributes!(:UnitsInStock => -amount)
+		product.update_attributes!(:UnitsOnOrder => amount)
+	end
 end

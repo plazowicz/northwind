2023d9192728dda3e91a8d967bc6fb5c92e4de55
Mateusz Opala <mateuszjacekopala@gmail.com>
2013-06-06 21:40:25 +0200

    Generate store

---
+++ app/models/store.rb
@@ -0,0 +1,5 @@
+class Store < ActiveRecord::Base
+  attr_accessible :owner
+
+  has_many :products
+end
---
+++ db/migrate/20130606190312_create_stores.rb
@@ -0,0 +1,9 @@
+class CreateStores < ActiveRecord::Migration
+  def change
+    create_table :stores do |t|
+      t.string :owner
+
+      t.timestamps
+    end
+  end
+end
